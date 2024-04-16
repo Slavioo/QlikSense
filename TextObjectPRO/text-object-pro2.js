865cdda06720b6d996568db6edbef8532200bb49
Slavioo <48221648+Slavioo@users.noreply.github.com>
2024-04-16 10:31:12 +0200

    Update text-object-pro2.js

--- TextObjectPRO/text-object-pro2.js
+++ TextObjectPRO/text-object-pro2.js
@@ -2,10 +2,9 @@ define(['jquery', 'qlik', 'css!./style.css'], function($, qlik) {
     return {
         initialProperties: {
             version: 1.0,
-            text: '',
+            html: '',
             css: '',
-            tooltip: '',
-            contentType: 'text' // Default content type set to 'text'
+            tooltip: ''
         },
         definition: {
             type: 'items',
@@ -14,37 +13,11 @@ define(['jquery', 'qlik', 'css!./style.css'], function($, qlik) {
                 settings: {
                     uses: 'settings',
                     items: {
-                        contentType: {
-                            type: 'string',
-                            component: 'dropdown',
-                            ref: 'contentType',
-                            label: 'Content Type',
-                            options: [{
-                                value: 'text',
-                                label: 'Text'
-                            }, {
-                                value: 'html',
-                                label: 'HTML'
-                            }],
-                            defaultValue: 'text'
-                        },
-                        text: {
-                            type: 'stringExpression',
-                            ref: 'text',
-                            label: 'Content',
-                            expression: 'optional',
-                            show: function(data) {
-                                return data.contentType === 'text'; // Only show this option if 'text' is selected
-                            }
-                        },
                         html: {
-                            type: 'stringExpression',
+                            type: 'string',
                             ref: 'html',
-                            label: 'HTML Content',
-                            expression: 'optional',
-                            show: function(data) {
-                                return data.contentType === 'html'; // Only show this option if 'html' is selected
-                            }
+                            label: 'HTML',
+                            expression: 'optional'
                         },
                         css: {
                             type: 'string',
@@ -63,18 +36,13 @@ define(['jquery', 'qlik', 'css!./style.css'], function($, qlik) {
             }
         },
         paint: function($element, layout) {
+            var html = layout.html;
             var css = layout.css;
             var tooltip = layout.tooltip;
 
             $element.attr('style', css);
             $element.attr('title', tooltip);
-
-            // Render content based on the selected content type
-            if (layout.contentType === 'html') {
-                $element.html(layout.html || ''); // Render HTML content
-            } else {
-                $element.text(layout.text || ''); // Render text content
-            }
+            $element.html(html);
         }
     };
 });
