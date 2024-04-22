e6656ff00e55c7c75196e19f86b18e4b35becc47
Slavioo <48221648+Slavioo@users.noreply.github.com>
2024-04-23 00:12:34 +0200

    Update export-button-pro2.js

--- TextObjectPRO/export-button-pro2.js
+++ TextObjectPRO/export-button-pro2.js
@@ -12,42 +12,42 @@ define(["qlik", "jquery"], function(qlik, $) {
                             ref: "visualizationId",
                             label: "Visualization ID",
                             defaultValue: "",
-							expression: 'optional'
+                            expression: 'optional'
                         },
                         pageSize: {
                             type: "integer",
                             ref: "pageSize",
                             label: "Page Size",
                             defaultValue: 500,
-							expression: 'optional'
+                            expression: 'optional'
                         },
                         csvDelimiter: {
                             type: "string",
                             ref: "csvDelimiter",
                             label: "CSV Delimiter",
                             defaultValue: ",",
-							expression: 'optional'
+                            expression: 'optional'
                         },
                         delay: {
                             type: "integer",
                             ref: "delay",
                             label: "Delay (ms)",
                             defaultValue: 100,
-							expression: 'optional'
+                            expression: 'optional'
                         },
                         fileNameMask: {
                             type: "string",
                             ref: "fileNameMask",
                             label: "File Name Mask",
                             defaultValue: "export",
-							expression: 'optional'
+                            expression: 'optional'
                         },
                         css: {
                             type: "string",
                             ref: "css",
                             label: "CSS",
                             defaultValue: "display: inline-block;outline: 0;text-align: center;cursor: pointer;padding: 0px 16px;border-radius: 2px;min-width: 80px;height: 32px;background-color: rgb(0, 120, 212);color: rgb(255, 255, 255);font-size: 14px;font-weight: 400;box-sizing: border-box;border: 1px solid rgb(0, 120, 212);",
-							expression: 'optional'
+                            expression: 'optional'
                         }
                     }
                 }
@@ -104,10 +104,10 @@ define(["qlik", "jquery"], function(qlik, $) {
                             Promise.all(promises).then(function() {
                                 $button.text('Completed');
                                 $button.prop('disabled', false);
-								$element.attr('title', 'Click to acknowledge. You  will be able to use the button again.');
+                                $element.attr('title', 'Click to acknowledge. You  will be able to use the button again.');
                                 $button.one('click', function() {
                                     $button.text('Export Data');
-									$element.attr('title', '');
+                                    $element.attr('title', '');
                                 });
                             });
                         });
