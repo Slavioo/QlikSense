0237de3f304f04c1b57aed68b319e177a4489fea
Slavioo <48221648+Slavioo@users.noreply.github.com>
2024-05-08 17:40:33 +0200

    Update export-button-pro4.js

--- TextObjectPRO/export-button-pro4.js
+++ TextObjectPRO/export-button-pro4.js
@@ -35,95 +35,93 @@ define(["qlik", "jquery"], function(qlik, $) {
                             type: "integer",
                             ref: "delay",
                             label: "Delay (ms)",
-                            defaultValue: 1
+                            defaultValue: 100
                         }
                     }
                 }
             }
         },
         paint: function($element, layout) {
-            var app = qlik.currApp(this);
-            var visualizationId = layout.visualizationId;
-            var pageSize = layout.pageSize;
-            var fileSize = layout.fileSize;
-            var csvDelimiter = layout.csvDelimiter;
-            var delay = layout.delay;
-            var $button = $('<button>Export Data</button>');
+            const app = qlik.currApp(this);
+            const $button = $('<button>Export Data</button>');
             $button.on('click', function() {
                 if ($button.text() === 'Export Data') {
-                    if (visualizationId) {
-                        $button.text('Exporting...');
-                        $button.prop('disabled', true);
-                        app.visualization.get(visualizationId).then(function(vis) {
-                            var numberOfPages = Math.ceil(vis.model.layout.qHyperCube.qSize.qcy / pageSize);
-                            var dimensionHeaders = vis.model.layout.qHyperCube.qDimensionInfo.map((dim, id) => ({
-                                Id: id,
-                                Header: dim.qFallbackTitle
-                            }));
-                            var measureHeaders = vis.model.layout.qHyperCube.qMeasureInfo.map((measure, id) => ({
-                                Id: id + dimensionHeaders.length,
-                                Header: measure.qFallbackTitle
-                            }));
-                            var allHeaders = dimensionHeaders.concat(measureHeaders);
-                            var orderedHeaders = vis.model.layout.qHyperCube.qColumnOrder.map(id => allHeaders.find(h => h.Id === id));
-                            var promises = [];
-                            for (var i = 0; i < numberOfPages; i += fileSize) {
-                                (function(startPage) {
-                                    var promise = new Promise(function(resolve, reject) {
-                                        setTimeout(function() {
-                                            var csvContent = orderedHeaders.filter(h => h.Header && h.Header.trim() !== '').map(h => h.Header).join(csvDelimiter) + '\n';
-                                            var filePromises = [];
-                                            for (var j = startPage; j < startPage + fileSize && j < numberOfPages; j++) {
-                                                (function(page) {
-                                                    var requestPage = [{
-                                                        qTop: page * pageSize,
-                                                        qLeft: 0,
-                                                        qWidth: vis.model.layout.qHyperCube.qSize.qcx,
-                                                        qHeight: Math.min(pageSize, vis.model.layout.qHyperCube.qSize.qcy - page * pageSize)
-                                                    }];
-                                                    var filePromise = vis.model.getHyperCubeData('/qHyperCubeDef', requestPage).then(function(dataPage) {
-                                                        csvContent += dataPage[0].qMatrix.map(row => row.map(cell => cell.qText).join(csvDelimiter)).join('\n');
-                                                        if (page < startPage + fileSize - 1 && page < numberOfPages - 1) {
-                                                            csvContent += '\n';
-                                                        }
-                                                    });
-                                                    filePromises.push(filePromise);
-                                                })(j);
-                                            }
-                                            Promise.all(filePromises).then(function() {
-                                                var csvFile = new Blob([csvContent], {
-                                                    type: "text/csv"
-                                                });
-                                                var downloadLink = document.createElement("a");
-                                                downloadLink.download = visualizationId + '_pages' + (startPage + 1) + '-' + (startPage + fileSize) + '.csv';
-                                                downloadLink.href = window.URL.createObjectURL(csvFile);
-                                                downloadLink.style.display = "none";
-                                                document.body.appendChild(downloadLink);
-                                                downloadLink.click();
-                                                resolve();
-                                            });
-                                        }, startPage * delay);
-                                    });
-                                    promises.push(promise);
-                                })(i);
-                            }
-                            Promise.all(promises).then(function() {
-                                $button.css('background-color', '#D0ECE7');
-                                $button.text('Completed');
-                                $button.prop('disabled', false);
-                                $button.one('click', function() {
-                                    $button.css('background-color', '');
-                                    $button.text('Export Data');
-                                });
-                            });
-                        });
-                    } else {
-                        console.error("No visualization id provided.");
-                    }
+                    exportData(app, layout, $button);
                 }
             });
             $element.empty();
             $element.append($button);
         }
+    };
+
+    function getHeaders(vis) {
+        const dimensionHeaders = vis.model.layout.qHyperCube.qDimensionInfo.map((dim, id) => ({
+            Id: id,
+            Header: dim.qFallbackTitle
+        }));
+        const measureHeaders = vis.model.layout.qHyperCube.qMeasureInfo.map((measure, id) => ({
+            Id: id + dimensionHeaders.length,
+            Header: measure.qFallbackTitle
+        }));
+        const allHeaders = dimensionHeaders.concat(measureHeaders);
+        const orderedHeaders = vis.model.layout.qHyperCube.qColumnOrder.map(id => allHeaders.find(h => h.Id === id));
+        const populatedHeaders = orderedHeaders.filter(h => h.Header && h.Header.trim() !== '');
+        return populatedHeaders;
+    };
+
+    async function exportData(app, layout, $button) {
+        const visualizationId = layout.visualizationId;
+        const pageSize = layout.pageSize;
+        const fileSize = layout.fileSize;
+        const csvDelimiter = layout.csvDelimiter;
+        const delay = layout.delay;
+
+        if (!visualizationId) {
+            console.error("No visualization id provided.");
+            return;
+        }
+
+        $button.text('Exporting...');
+        $button.prop('disabled', true);
+
+        const vis = await app.visualization.get(visualizationId);
+        const numberOfPages = Math.ceil(vis.model.layout.qHyperCube.qSize.qcy / pageSize);
+        const headers = getHeaders(vis);
+
+        for (let startPage = 0; startPage < numberOfPages; startPage += fileSize) {
+            let csvContent = headers.map(h => h.Header).join(csvDelimiter) + '\n';
+
+            for (let page = startPage; page < startPage + fileSize && page < numberOfPages; page++) {
+                const requestPage = [{
+                    qTop: page * pageSize,
+                    qLeft: 0,
+                    qWidth: vis.model.layout.qHyperCube.qSize.qcx,
+                    qHeight: Math.min(pageSize, vis.model.layout.qHyperCube.qSize.qcy - page * pageSize)
+                }];
+                const dataPage = await vis.model.getHyperCubeData('/qHyperCubeDef', requestPage);
+                csvContent += dataPage[0].qMatrix.map(row => row.map(cell => cell.qText).join(csvDelimiter)).join('\n');
+                if (page < startPage + fileSize - 1 && page < numberOfPages - 1) {
+                    csvContent += '\n';
+                }
+            }
+
+            const csvFile = new Blob([csvContent], { type: "text/csv" });
+            const downloadLink = document.createElement("a");
+            downloadLink.download = visualizationId + '_pages' + (startPage + 1) + '-' + (startPage + fileSize) + '.csv';
+            downloadLink.href = window.URL.createObjectURL(csvFile);
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+
+            await new Promise(resolve => setTimeout(resolve, delay));
+        }
+
+        $button.css('background-color', '#D0ECE7');
+        $button.text('Completed');
+        $button.prop('disabled', false);
+        $button.one('click', function() {
+            $button.css('background-color', '');
+            $button.text('Export Data');
+        });
     };
 });
