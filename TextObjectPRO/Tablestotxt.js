305e049ab323f8f987b2d65fec4ca39046281fb5
Slavioo <48221648+Slavioo@users.noreply.github.com>
2024-05-24 11:21:54 +0200

    Create Tablestotxt.js

---
+++ TextObjectPRO/Tablestotxt.js
@@ -0,0 +1,35 @@
+function downloadCustomTablesAsTXT(filename) {
+  var txt = [];
+  var tables = document.querySelectorAll(".table-container .custom-table"); // Only select tables with class 'custom-table'
+  
+  tables.forEach(function(table, index) {
+    txt.push("Table " + (index + 1));
+    var rows = table.querySelectorAll("tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+      var row = [], cols = rows[i].querySelectorAll("td, th");
+      
+      for (var j = 0; j < cols.length; j++) {
+        var data = cols[j].innerText.replace(/"/g, '""');
+        data = '"' + data + '"';
+        row.push(data);
+      }
+      
+      txt.push(row.join("\\t")); // Use tab as the separator
+    }
+    
+    txt.push(""); // Add an empty line to separate tables
+  });
+  
+  var txtFile = new Blob([txt.join("\\n")], {type: "text/plain"});
+  var downloadLink = document.createElement("a");
+  downloadLink.download = filename;
+  downloadLink.href = window.URL.createObjectURL(txtFile);
+  downloadLink.style.display = "none";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+// Update the button's onclick event to use the new function
+document.getElementById('download-btn').setAttribute('onclick', "downloadCustomTablesAsTXT('custom_tables.txt')");
